f36253eb440a0d280afd95faba35ed30865e96a8
Indrajit Karmaker <indrajitkarmaker87@gmail.com>
2024-04-03 22:36:54 +0600

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Header from "./components/header";
+
 function App() {
   return (
     <>
-      <h1>hello</h1>
+      <Header/>
     </>
   );
 }
---
+++ src/components/header.jsx
@@ -0,0 +1,36 @@
+function getDate() {
+  const today = new Date();
+  const month = today.getMonth() + 1;
+  const year = today.getFullYear();
+  const date = today.getDate();
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  return `${date}-${months[month - 1]}-${year}`;
+}
+console.log(getDate());
+
+const Header = () => {
+  return (
+    <>
+      <div className="text-center my-5 space-y-1">
+        <h1 className="text-4xl">The Dragon News</h1>
+        <h1 className="text-md">Journalism Without Fear or Favour</h1>
+        <h1 className="font-bold">{getDate()}</h1>
+      </div>
+    </>
+  );
+};
+
+export default Header;
